eca8b96f031974fd13f5854b66958fb1022a6c04
rozhiar <rozyar.313175000@uhd.edu.iq>
2022-11-11 02:02:45 +0300

    Completed assigment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Add your code here
+
+function submitData(userName, userEmail) {
+    let formData = {
+        name: `${userName}`,
+        email: `${userEmail}`
+    };
+
+    fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify(formData)
+        
+    }).then((response) => {
+        // console.log(response.json);
+        return response.json();
+    }).then((object) => {
+        console.log(object);
+        document.body.innerHTML = object.id;
+
+    
+    }).catch((err) =>{
+        document.body.innerHTML = err.message;
+
+    });
+    
+    
+    };
+
+    // submitData('Steve', 'hello@gmail.com')
